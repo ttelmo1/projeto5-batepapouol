e230ddc3207f65fb1eb674e6ca96042416866160
ttelmo1 <telmobsj@gmail.com>
2022-10-27 13:10:53 -0300

    Envio com Enter e outros bônus

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ login().then(response => {
 
 
 msgChegou();
-
-//Atualizar as mensagens
+renderizarParticipantes();
+// Atualizar as mensagens
 // setInterval(function(){
 //     atualizarMsg()
 // }, 3000)
@@ -17,6 +17,14 @@ msgChegou();
 setInterval(function () {
     atualizarStatus()
 }, 5000)
+
+
+// setInterval(function () {
+//     let participantes = document.getElementById("online");
+//     participantes.innerHTML = "";
+//     renderizarParticipantes();
+// }, 10000)
+
 
 function atualizarStatus(){
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status",{
@@ -37,7 +45,6 @@ function msgChegou(resposta) {
             message = resposta.data;
             //Chamando as funções de renderização
             renderizarMsg();
-            renderizarParticipantes();
         }
     )
 }
@@ -51,16 +58,29 @@ function login() {
 
 //Renderizar Participantes na barra lateral
 function renderizarParticipantes() {
-    let participantes = document.querySelector(".contatos");
-    for (let i = 0; i < 10; i++) {
-        let participante = message[i];
-        participantes.innerHTML += `
-        <li class="participante">
-            <ion-icon name="person-circle-outline"></ion-icon>
-            ${participante.from}
-        </li>`;
-    }
+    axios.get("https://mock-api.driven.com.br/api/v6/uol/participants").then(response => {
+        let participantes = document.getElementById("online");
+        console.log(response)
+        for (let i = 0; i < response.data.length; i++) {
+            let participante = response.data[i];
+            console.log(participante)
+            participantes.innerHTML += `
+            <li onclick="marcar()" data-identifier="participant" class="participante">
+                <ion-icon name="person-circle-outline"></ion-icon>
+                ${participante.name}
+                <div class="check"></div>
+                <ion-icon id="check" name="checkmark"></ion-icon>
+            </li>`;
+        }
+    })
+    
 }
+
+function marcar(){
+    document.getElementById("check").style.visibility="visible"
+}
+
+
 
 //Enviar mensagem e renderizar na conversa
 function sendMsg() {
@@ -96,7 +116,7 @@ function renderizarMsg() {
                 <span class="usuario">${mensagemEnviada.from}</span>
                 para
                 <span class="destinatario">${mensagemEnviada.to}</span>:
-                ${mensagemEnviada.text}
+                <span class="conteudo">${mensagemEnviada.text}</span>
             </li>`;
 
         }
@@ -124,3 +144,13 @@ function fecharSideBar() {
     document.querySelector(".back").style.transition = "all 0.4s";
     document.querySelector(".back").style.transition = "ease-out";
 }
+
+window.onload = function(){
+    const elem = document.getElementById("inputMessage")
+    elem.addEventListener("keypress", function(e){
+        if(e.keyCode == 13){
+           e.preventDefault();
+           document.getElementById("sendButton").click();
+        }
+    })
+};
